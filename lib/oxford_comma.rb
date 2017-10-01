dabf6cd28a969660dfbdd947ba092d95a561c7e4
Araba Taylor <araba.taylor20@gmail.com>
2017-10-01 19:08:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
+array = [1,2,3]
+
 def oxford_comma(array)
   if array.length==1
     array.join
@@ -5,8 +7,5 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length==3
     array.slice(2)
-    string_one = array[0].join(",")
-    string_two = array[0][1].join(" and")
-    string_one.push(string_two.flatten!)
   end
 end
