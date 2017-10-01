d6fa8c3dd8c6bb0658d84178217af75d2eea5ad8
Araba Taylor <araba.taylor20@gmail.com>
2017-10-01 18:53:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,8 @@ def oxford_comma(array)
     array.join
   elsif array.length==2
     array.join(" and ")
-  elsif array.length==3
-    array.join(", ")
-  else array.length>3
+  elsif array.length==3 || array.length>3
+    array[-2]<<and
     array.join(", ")
   end
 end
